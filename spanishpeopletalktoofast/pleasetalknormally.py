4b9d90dd712caaff34310dbf856614971fba7956
Nicolas Loew <ympker@web.de>
2021-07-01 14:55:51 +0200

    Create pleasetalknormally.py

---
+++ spanishpeopletalktoofast/pleasetalknormally.py
@@ -0,0 +1,5 @@
+#Spanish people talk way too fast. That is why a dutch and a german student decided to fix that bug commiting a fix.
+#Pseudocode, bugs and some actually code follows here :)
+
+#While Spanish People talk, make sure they don't talk too fast. If they talk too fast (and they always do), slow down.
+#Startcoding please
